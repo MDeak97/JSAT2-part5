0ce5d41ff907ff7bc0d8724b19edd073f296814b
MDeak97 <michaeldeyak90@gmail.com>
2025-10-24 11:14:19 +1100

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,4 @@
 
 // Q5.1 Record GitHub repository details for this part - please see attached word document.
 
-//
+// Q5.2 Gather the UI requirements from the description above and communicate with the client. Confirm the requirements with your client and obtain client sign off - please see attached word document.
