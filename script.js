f2f41d86c731319bfd4520693e66ab1746f564eb
MDeak97 <michaeldeyak90@gmail.com>
2025-10-24 11:10:57 +1100

    Answering Q5.1

---
+++ script.js
@@ -0,0 +1,9 @@
+// File: script.js
+// Description: File to answer questions from Part 5 of assessment JSAT 2
+// Author: Michael Deak
+// Date: 24/10/2025
+// Version: 1.00
+
+// Q5.1 Record GitHub repository details for this part - please see attached word document.
+
+//
