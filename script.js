ccdc6525be00b030b62199cc20ac9e8d07313e35
MDeak97 <michaeldeyak90@gmail.com>
2025-10-24 13:20:35 +1100

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,5 @@
 // Q5.4 - What are the software applications that are to be used to create the prototype UI (wireframe and prototype) - please see attached word document.
 
 // Q5.5 - Build the wireframe of your UI using the tool you identified in question 5.4 - please see attached word document to see screenshot of wireframe.
+
+// Q5.6 - Review the wireframe with the client and get feedback. Revise and update the wireframe from feedback given - please see attached word document.
