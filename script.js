0c706f932b09721bb9083a46489e7886ca800156
MDeak97 <michaeldeyak90@gmail.com>
2025-10-24 14:07:43 +1100

    Q5.9

--- script.js
+++ script.js
@@ -19,3 +19,5 @@
 // Q5.7 - Create the theme for your UI. Record the colours and font below - please see attached word document.
 
 // Q5.8 - Create prototype of the application using tools listed in Q5.4. Enhance the wireframe with the theme colours - please see attached word document to see screenshot of prototype.
+
+// Q5.9 - Review the prototype with the client and get feedback. Revise and update the wireframe from feedback given. Obtain signoff - please see attached word document.
