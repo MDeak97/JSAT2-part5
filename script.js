74f0ae7720155bf29180d0c6ea06a36f87f19550
MDeak97 <michaeldeyak90@gmail.com>
2025-11-15 21:07:59 +1100

    Answering Q5.11

--- script.js
+++ script.js
@@ -28,4 +28,7 @@
 // Q5.10 - Describe the content flow / UI Actions / UI Events of each of the UI sections required - please see attached word document
 
 // Q5.11 Determine the languages used to build the UI. List all the languages used and describe what they are used for. (at least 1 paragraph for each description) - please see attached word document
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
