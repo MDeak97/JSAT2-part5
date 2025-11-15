60fc6b081f7ac322ccb6f93757d0615a0b64e13e
MDeak97 <michaeldeyak90@gmail.com>
2025-11-15 21:12:51 +1100

    Update script.js

--- script.js
+++ script.js
@@ -3,27 +3,3 @@
 // Author: Michael Deak
 // Date: 15/11/2025
 // Version: 1.00
-
-// Q5.1 - Record GitHub repository details for this part - please see attached word document.
-
-// Q5.2 - Gather the UI requirements from the description above and communicate with the client. Confirm the requirements with your client and obtain client sign off - please see attached word document.
-
-// Q5.3 - What are the HTML guideline documents that are to be followed? List the filename or URL of the guidelines. Describe the guidelines you are following (at least 1 paragraph) - please see attached word document
-
-// Q5.4 - What are the software applications that are to be used to create the prototype UI (wireframe and prototype) - please see attached word document.
-
-// Q5.5 - Build the wireframe of your UI using the tool you identified in question 5.4 - please see attached word document to see screenshot of wireframe.
-
-// Q5.6 - Review the wireframe with the client and get feedback. Revise and update the wireframe from feedback given - please see attached word document.
-
-// Q5.7 - Create the theme for your UI. Record the colours and font below - please see attached word document.
-
-// Q5.8 - Create prototype of the application using tools listed in Q5.4. Enhance the wireframe with the theme colours - please see attached word document to see screenshot of prototype.
-
-// Q5.9 - Review the prototype with the client and get feedback. Revise and update the wireframe from feedback given. Obtain signoff - please see attached word document.
-
-// Q5.10 - Describe the content flow / UI Actions / UI Events of each of the UI sections required - please see attached word document.
-
-// Q5.10 - Describe the content flow / UI Actions / UI Events of each of the UI sections required - please see attached word document
-
-// Q5.11 Determine the languages used to build the UI. List all the languages used and describe what they are used for. (at least 1 paragraph for each description) - please see attached word document
